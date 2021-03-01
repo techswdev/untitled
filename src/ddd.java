7f90809f1164ea4c2d0532fb34471bd83788df07
Bc A <bca@Bcui-MacBookPro.local>
2021-03-01 19:56:32 +0900

    Initial commit

---
+++ src/ddd.java
@@ -0,0 +1,8 @@
+public class ddd {
+
+    private String dddd;
+
+    protected ddd(String dddd) {
+        this.dddd = dddd;
+    }
+}
---
+++ src/main.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class main {
+
+    public static void main(String[] args) {
+        Map<String,Object> map1 =  new HashMap<>();
+        map1.get("aa1");
+
+
+        Map<String,Object> map2 =  new HashMap<>();
+        map2.get("aa2");
+
+    }
+}
